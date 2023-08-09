0e24ba32a845fc6bee16d379427bf5a957a196f0
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-08-09 23:53:01 +0800

    Associate edit functionality

---
+++ app/api/associate/[associateId]/route.ts
@@ -0,0 +1,56 @@
+import prismadb from "@/lib/prismadb";
+import { currentUser } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { associateId: string } }
+) {
+  try {
+    const body = await req.json();
+    const user = await currentUser();
+    const { src, name, description, instructions, seed, categoryId } = body;
+
+    if (!params.associateId) {
+      return new NextResponse("Associate ID is required", { status: 400 });
+    }
+
+    if (!user || !user.id || !user.firstName) {
+      return new NextResponse("Unauthorized", { status: 400 });
+    }
+
+    if (
+      !src ||
+      !name ||
+      !description ||
+      !instructions ||
+      !seed ||
+      !categoryId
+    ) {
+      return new NextResponse("Missing required fields", { status: 400 });
+    }
+
+    //TODO: Check for subscription
+
+    const associate = await prismadb.associate.update({
+      where: {
+        id: params.associateId,
+      },
+      data: {
+        categoryId,
+        userId: user.id,
+        userName: user.firstName,
+        src,
+        name,
+        description,
+        instructions,
+        seed,
+      },
+    });
+
+    return NextResponse.json(associate);
+  } catch (error) {
+    console.log("[ASSOCIATE_PATCH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
