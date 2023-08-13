39459c16f2bd4a22279589a6a60c8e683245625b
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-08-13 13:06:21 +0800

    Installed ai package, Updated chat client, and added chat form

--- app/(chat)/(routes)/chat/[chatId]/components/ChatClient.tsx
+++ app/(chat)/(routes)/chat/[chatId]/components/ChatClient.tsx
@@ -1,7 +1,12 @@
 "use client";
 
-import { ChatHeader } from "@/components/ChatHeader";
+import { useCompletion } from "ai/react";
+import { FormEvent, useState } from "react";
 import { Associate, Message } from "@prisma/client";
+import { useRouter } from "next/navigation";
+
+import { ChatHeader } from "@/components/ChatHeader";
+import { ChatForm } from "@/components/ChatForm";
 
 interface IChatClientProps {
   associate: Associate & {
@@ -13,9 +18,44 @@ interface IChatClientProps {
 }
 
 export const ChatClient = ({ associate }: IChatClientProps) => {
+  const router = useRouter();
+  const [messages, setMessages] = useState<any[]>(associate.messages);
+  const { input, isLoading, handleInputChange, handleSubmit, setInput } =
+    useCompletion({
+      api: `/api/chat/${associate.id}`,
+      onFinish(prompt, completion) {
+        const systemMessage = {
+          role: "system",
+          content: completion,
+        };
+
+        setMessages((current) => [...current, systemMessage]);
+        setInput("");
+
+        router.refresh();
+      },
+    });
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    const userMessage = {
+      role: "system",
+      content: input,
+    };
+
+    setMessages((current) => [...current, userMessage]);
+
+    handleSubmit(e);
+  };
+
   return (
     <div className="flex flex-col h-full p-4 space-y-2">
       <ChatHeader associate={associate} />
+      <ChatForm
+        input={input}
+        handleInputChange={handleInputChange}
+        onSubmit={onSubmit}
+        isLoading={isLoading}
+      />
     </div>
   );
 };
