2819c4461cdfdaf4cf202c5f6e958717f8f903d5
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-08-15 21:50:24 +0800

    Added fake loading in the initial bot message

--- components/ChatMessages.tsx
+++ components/ChatMessages.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { Associate } from "@prisma/client";
 import { ChatMessage, IChatMessageProps } from "./ChatMessage";
 
@@ -14,16 +15,26 @@ export const ChatMessages = ({
   isLoading,
   messages = [],
 }: IChatMessagesProps) => {
+  const [fakeLoading, setFakeLoading] = useState(
+    messages.length === 0 ? true : false
+  );
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setFakeLoading(false);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
   return (
     <div className="flex-1 overflow-y-auto pr-4">
       <ChatMessage
+        isLoading={fakeLoading}
         src={associate.src}
         role="system"
-        content={`Hello I am ${associate.name}, ${associate.description}`}
-      />
-      <ChatMessage
-        src={associate.src}
-        role="user"
         content={`Hello I am ${associate.name}, ${associate.description}`}
       />
     </div>
